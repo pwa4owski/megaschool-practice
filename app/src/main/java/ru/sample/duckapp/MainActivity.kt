79c2f20c9243d4458073ad475176fcc0dcfb72a2
pwa4owski <312431@niuitmo.ru>
2024-02-17 16:35:49 +0300

    part2 done

--- app/src/main/java/ru/sample/duckapp/MainActivity.kt
+++ app/src/main/java/ru/sample/duckapp/MainActivity.kt
@@ -1,23 +1,28 @@
 package ru.sample.duckapp
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import ru.sample.duckapp.data.DucksApi
 import ru.sample.duckapp.domain.Duck
 import ru.sample.duckapp.infra.Api
+import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
 
     private val ducksApi: DucksApi = Api.ducksApi
-    lateinit var imageView: ImageView
-
+    private lateinit var imageView: ImageView
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +30,35 @@ class MainActivity : AppCompatActivity() {
 
         imageView = findViewById(R.id.imageView)
 
-        loadImage("https://random-d.uk/api/135")
+        editText = findViewById(R.id.editCode)
 
-        val btn = findViewById<Button>(R.id.button)
+        randomRequest()
 
-        btn.setOnClickListener {
+        findViewById<Button>(R.id.button).setOnClickListener {
             onClick()
         }
 
     }
 
-    private fun loadImage(imageUrl: String?) {
-        imageUrl?.let {
-            Picasso.get().load(it).into(imageView)
+
+    private fun onClick() {
+        val txt: String = editText.text.toString()
+        if(txt.isBlank()) {
+            randomRequest()
+            return
+        }
+        try {
+            val code = txt.toInt()
+            if(code < 100 || code >= 600)
+                showValidationPopup("Введите целое число в интервале [100; 600)")
+            else
+                httpCodeRequest(code)
+        } catch (e: NumberFormatException) {
+            showValidationPopup("Ну для начала хоть число введите...")
         }
     }
 
-    private fun onClick() {
+    private fun randomRequest() {
         val call: Call<Duck> = ducksApi.getRandomDuck()
 
         call.enqueue(object : Callback<Duck> {
@@ -50,13 +67,60 @@ class MainActivity : AppCompatActivity() {
                     val imageUrl = response.body()?.url
                     loadImage(imageUrl)
                 } else {
-                    error("Oops.. Some error occurred:(")
+                    showValidationPopup("Упс..")
                 }
             }
 
             override fun onFailure(call: Call<Duck>, e: Throwable) {
-                e.printStackTrace()
+                showValidationPopup("Что-то не так не с сетью...")
             }
         })
+    }
+
+    private fun httpCodeRequest(code: Int) {
+
+        val call: Call<ResponseBody> = ducksApi.getHttpCode(code)
+
+        call.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) {
+                    val imageStream = response.body()?.byteStream()
+                    loadImage(imageStream)
+                } else {
+                    showValidationPopup("Кажется, такой утки нет(")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                showValidationPopup("Что-то не так не с сетью...")
+            }
+        })
+    }
+
+
+    private fun loadImage(imageByteStream: InputStream?) {
+        imageByteStream?.let {
+            val bitmap = BitmapFactory.decodeStream(imageByteStream)
+            imageView.setImageBitmap(bitmap)
+        }
+    }
+
+    private fun loadImage(imageUrl: String?) {
+        imageUrl?.let {
+            Picasso.get().load(it).into(imageView)
+        }
+    }
+
+    fun showValidationPopup(message: String) {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+
+        alertDialogBuilder.setMessage(message)
+
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
--- app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
+++ app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
@@ -1,10 +1,15 @@
 package ru.sample.duckapp.data
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import ru.sample.duckapp.domain.Duck
 
 interface DucksApi {
     @GET("random")
     fun getRandomDuck(): Call<Duck>
+
+    @GET("http/{code}")
+    fun getHttpCode(@Path("code") code: Int): Call<ResponseBody>
 }
